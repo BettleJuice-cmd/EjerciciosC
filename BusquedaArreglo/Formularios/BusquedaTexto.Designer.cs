252830efec5c56e0c2a592e45e17666d333fae56
BettleJuice <asdiazm@uamv.edu.ni>
2025-11-23 22:30:03 -0600

    Termine el ejercicio 3, funciona correctamente.

--- BusquedaArreglo/Formularios/BusquedaTexto.Designer.cs
+++ BusquedaArreglo/Formularios/BusquedaTexto.Designer.cs
@@ -29,35 +29,26 @@
         private void InitializeComponent()
         {
             this.tbParrafo = new System.Windows.Forms.TextBox();
-            this.btnAgregar = new System.Windows.Forms.Button();
             this.lblParrafo = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tbPalabra = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbParrafo
             // 
-            this.tbParrafo.Location = new System.Drawing.Point(162, 64);
+            this.tbParrafo.Location = new System.Drawing.Point(203, 64);
             this.tbParrafo.Name = "tbParrafo";
             this.tbParrafo.Size = new System.Drawing.Size(100, 22);
             this.tbParrafo.TabIndex = 0;
             // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(347, 62);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(109, 27);
-            this.btnAgregar.TabIndex = 1;
-            this.btnAgregar.Text = "AGREGAR";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
-            // 
             // lblParrafo
             // 
             this.lblParrafo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblParrafo.Location = new System.Drawing.Point(30, 115);
+            this.lblParrafo.Location = new System.Drawing.Point(30, 178);
             this.lblParrafo.Name = "lblParrafo";
             this.lblParrafo.Size = new System.Drawing.Size(426, 153);
             this.lblParrafo.TabIndex = 3;
@@ -75,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 313);
+            this.label2.Location = new System.Drawing.Point(27, 114);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(170, 16);
             this.label2.TabIndex = 5;
@@ -83,31 +74,53 @@
             // 
             // tbPalabra
             // 
-            this.tbPalabra.Location = new System.Drawing.Point(203, 310);
+            this.tbPalabra.Location = new System.Drawing.Point(203, 114);
             this.tbPalabra.Name = "tbPalabra";
             this.tbPalabra.Size = new System.Drawing.Size(100, 22);
             this.tbPalabra.TabIndex = 6;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(347, 305);
+            this.btnBuscar.Location = new System.Drawing.Point(358, 112);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(109, 27);
             this.btnBuscar.TabIndex = 7;
             this.btnBuscar.Text = "BUSCAR";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(358, 62);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(109, 27);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(339, 334);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(117, 41);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // BusquedaTexto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.tbPalabra);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblParrafo);
-            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.tbParrafo);
             this.Name = "BusquedaTexto";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -121,11 +134,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox tbParrafo;
-        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Label lblParrafo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbPalabra;
         private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
--- BusquedaArreglo/Formularios/BusquedaTexto.cs
+++ BusquedaArreglo/Formularios/BusquedaTexto.cs
@@ -19,14 +19,54 @@ namespace BusquedaArreglo.Formularios
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            lblParrafo.Text = "PARRAFO AGREGADO: ";
-            string parrafo = tbParrafo.Text;
-           
-            lblParrafo.Text+= parrafo;
+            
 
 
 
 
+        }
+        public int ContarPalabrasManual(string parrafo, string palabra)
+        {
+            
+            if (string.IsNullOrEmpty(parrafo) || string.IsNullOrEmpty(palabra))
+            {
+                return 0;
+            }
+
+            if (palabra.Length > parrafo.Length)
+            {
+                return 0; 
+            }
+
+            int contador = 0;
+
+            
+            for (int i = 0; i <= parrafo.Length - palabra.Length; i++)
+            {
+                bool coincide = true;
+
+                
+                for (int j = 0; j < palabra.Length; j++)
+                {
+                    
+                    char letraTexto = char.ToLower(parrafo[i + j]);
+                    char letraPalabra = char.ToLower(palabra[j]);
+
+                    if (letraTexto != letraPalabra)
+                    {
+                        coincide = false;
+                        break; 
+                    }
+                }
+
+               
+                if (coincide)
+                {
+                    contador++;
+                }
+            }
+
+            return contador;
         }
 
         private void BusquedaTexto_Load(object sender, EventArgs e)
@@ -37,6 +77,34 @@ namespace BusquedaArreglo.Formularios
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string parrafo = tbParrafo.Text; 
+            string palabra = tbPalabra.Text; 
+
+            int veces = ContarPalabrasManual(parrafo, palabra);
+
+            MessageBox.Show($"La palabra '{palabra}' aparece {veces} veces.", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            tbParrafo.Clear();
+            tbPalabra.Clear();
+
+            
+        }
+
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            lblParrafo.Text = "PARRAFO AGREGADO: ";
+            string parrafo = tbParrafo.Text;
+
+            lblParrafo.Text += parrafo;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblParrafo.Text = "";
         }
     }
 }
